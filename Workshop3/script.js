9ecb399f8901b4753eaae379e7e95c0727d6de00
Simon Pradhananga <107235851+SimonPradhan@users.noreply.github.com>
2022-06-30 17:47:03 +0000

    Add question 8 and 9

--- Workshop3/script.js
+++ Workshop3/script.js
@@ -1,17 +1,24 @@
+//Question no. 1
 function triArea(base, height) {
     return (base* height)/2;
 }
+triArea(3,4);
 
+//Question no. 2
 function giveMeSomething(a){
     something = 'something' +" "+ a;
     return something;
 }
+giveMeSomething("is nothing.");
 
+// Question no. 3
 function basketballScore(two,three){
     totalpoint = two * 2 + three * 3;
     return totalpoint;
 }
+basketballScore(4,3);
 
+//Question no. 4
 function lessThan100(n1,n2){
     totalsum = n1 + n2;
     if (totalsum < 100){
@@ -20,7 +27,9 @@ function lessThan100(n1,n2){
         return false;
     }
 }
+lessThan100(10,20);
 
+//Question no. 5
 function addup(n){
    var sum= 0;
     for (i= 1; i <= n; i++){
@@ -28,32 +37,9 @@ function addup(n){
     }
     return sum;
 }
-//     i = 0
-//        while (i <= n){
-//        sum += i
-//        i += 1
-//     }
-// }
+addup(5);
 
-// function oddishOrEvenish(a){
-//     sum = 0;
-//  while (a!= 0){
-//     var R = a % 10;
-//     sum = sum + R;
-//     a= Math.floor(a/10);
-
-//  }
-//  if (sum%2 == 0){
-//     document.write("Evenish")
-//  }else{
-//     document.write("Oddish")
-//  }
-// }
-
-function primeInRange(n1,n2){
-    for()
-
-}
+//Question no. 6
 function oddishOrEvenish(a){
     sum = 0;
     for (i= 0;  a!= 0; i++){
@@ -67,4 +53,41 @@ function oddishOrEvenish(a){
         console.log("Oddish")
     }
 }
-oddishOrEvenish(12)
+// oddishOrEvenish(12);
+
+//Question no. 7
+function primeInRange(n1){
+    count = 0
+    while (i<=n1){
+        if (n1 % i == 0){
+            count += 1
+            if (count ==2){
+                return true;
+            }else{
+                return false;
+            }break
+        }
+    i += 1
+    }
+}
+primeInRange(11);
+
+
+//Question no. 8
+function shiftToLeft(n,m){
+    result = n * (2 ** m);
+    return result
+}
+
+//Question no. 9
+function binary(n){
+    i = 1 ;
+    result = 0;
+    while (n == 0 ){
+        R = n % 2;
+        n = Math.floor(n/2);
+
+        result += R;
+    }
+console.log(result);
+}
