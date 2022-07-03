ffe7da8e3b6a67ac6f033c7eb4a0f91508094ad2
Simon Pradhananga <107235851+SimonPradhan@users.noreply.github.com>
2022-07-03 18:30:39 +0000

    completed workshop 3

--- Workshop3/script.js
+++ Workshop3/script.js
@@ -1,26 +1,26 @@
 //Question no. 1
 function triArea(base, height) {
-    return (base* height)/2;
+    return (base* height)/2;    //returns the area of triangle by applying the formula 
 }
 triArea(3,4);
 
 //Question no. 2
-function giveMeSomething(a){
+function giveMeSomething(a){        // this function adds "sommething" to the value given
     something = 'something' +" "+ a;
     return something;
 }
 giveMeSomething("is nothing.");
 
 // Question no. 3
-function basketballScore(two,three){
+function basketballScore(two,three){       //this function adds the basketball score according to the 3 point and 2 point 
     totalpoint = two * 2 + three * 3;
     return totalpoint;
 }
 basketballScore(4,3);
 
 //Question no. 4
-function lessThan100(n1,n2){
-    totalsum = n1 + n2;
+function lessThan100(n1,n2){    //this function checks the total sum of the given parameter and 
+    totalsum = n1 + n2;         // returns true if sum is less than 100 else returns false
     if (totalsum < 100){
         return true;
     }else{
@@ -30,7 +30,7 @@ function lessThan100(n1,n2){
 lessThan100(10,20);
 
 //Question no. 5
-function addup(n){
+function addup(n){      //this function adds the number from starting up to the number given
    var sum= 0;
     for (i= 1; i <= n; i++){
         sum += i;
@@ -40,12 +40,12 @@ function addup(n){
 addup(5);
 
 //Question no. 6
-function oddishOrEvenish(a){
-    sum = 0;
+function oddishOrEvenish(a){    //this function  adds the digits of the given number and checks whether the
+    sum = 0;                    // sum is oddish or evenish
     for (i= 0;  a!= 0; i++){
         var R = a % 10;
         sum += R;
-        a = Math.floor(a/10);
+        a = Math.floor(a/10);   //does the truncate division 
     }
     if (sum%2 == 0){
         console.log("Evenish")
@@ -54,33 +54,36 @@ function oddishOrEvenish(a){
     }
 }
 
-function primeInRange(n1, n2) {
-    count = 0; 
-    for (i = 1; n1 <= n2; i++){
-        for (j = 1; j<  (n1/2)+1; j++){
-            if (n1  % j == 0){
-                count += 1;
-            } break
-        }n1+=1;
-    }
-    if (count == 2){
-        return true;
-    }else{
-        return false;
-    }
+    
+function primeInRange(n1, n2) {         //this function checks the range and returns true if the range contains at least one prime number 
+    num1 = n1;   
+    num2 = n2;
+
+    while (num1 <= num2) {
+        let i = 2;
+        while (i < num1 / 2) {
+            if (num1 % i == 0) {
+                console.log(true);
+                break
+            }
+            i++;
+        }
+        num1++;
+    };
 };
 
+primeInRange(5, 11);
 
 
 
 //Question no. 8
-function shiftToLeft(n,m){
+function shiftToLeft(n,m){      //this function multiplies the value 'n' with two to the power 'm'
     result = n * (2 ** m);
     return result
 }
 
 //Question no. 9
-function binary(n){
+function binary(n){         //this function changes the given decimal value into binary system
 decimal = n.toString();
 let binary = "";
 while (n>=1){
