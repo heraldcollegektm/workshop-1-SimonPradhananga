12dda2c3550569a1696fa09eade03b77c621f3ac
Simon Pradhananga <107235851+SimonPradhan@users.noreply.github.com>
2022-07-04 04:55:08 +0000

    created new folder workshop 4 and added index.html and script,js file and added various codes

---
+++ Workshop4/script.js
@@ -0,0 +1,28 @@
+function printArray(number){
+    var newArray = [];
+    for (var i = 1; i <= number; i++){
+        newArray.push(i);
+    }
+    return newArray;
+};
+console.log(printArray(6));
+printArray(1);
+ //Qn 2 
+
+ function countTrue(a){
+    // newArray = [a];
+    count = 0;
+    for (var i = 0; i <= a.length;i++){
+        if ( a[i] ){
+            count +=1
+        }
+    }
+    console.log(count);
+ }
+
+ //Qn 3
+ function toArray(object){
+    for ([key,value] in Object.entries(object)){
+        console.log(Object.entries(object));
+    }
+ }
